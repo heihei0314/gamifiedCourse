dab72f091d7fb2f3b8160195fa00a8a0b31e3d42
Tsz Hei YEUNG <heihei0314@gmail.com>
2025-02-19 16:19:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,55 +1,7 @@
 
 <?php
     error_reporting(E_ALL ^ E_DEPRECATED);
-    
-    /*
-     * Example that changes html of phpcas messages
-     *
-     * PHP Version 7
-     *
-     * @file     example_html.php
-     * @category Authentication
-     * @package  PhpCAS
-     * @author   Joachim Fritschi <jfritschi@freenet.de>
-     * @author   Adam Franco <afranco@middlebury.edu>
-     * @license  http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
-     * @link     https://wiki.jasig.org/display/CASC/phpCAS
-     */
-      /*
-    // Load the settings from the central config file
-    require_once 'model/config.php';
-    // Load the CAS lib
-    require_once $phpcas_path . '/CAS.php';
-    
-    // Initialize phpCAS
-    phpCAS::client(CAS_VERSION_3_0,'cas.ust.hk',443,'cas');
-    
-    // For production use set the CA certificate that is the issuer of the cert
-    // on the CAS server and uncomment the line below
-    phpCAS::setCasServerCACert('model/cachain.pem');
-    
-    // For quick testing you can disable SSL validation of the CAS server.
-    // THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
-    // VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
-    phpCAS::setNoCasServerValidation();
-    
-    // force CAS authentication
-    phpCAS::forceAuthentication();
-    
-    // at this step, the user has been authenticated by the CAS server
-    // and the user's login name can be read with phpCAS::getUser().
-    
-    // logout if desired
-    if (isset($_REQUEST['logout'])) {
-        phpCAS::logout();
-    }
-    
-     
-       /*
-    $uid=phpCAS::getUser();
-    $userInfo = phpCAS::getAttributes();
-    $fullName = $userInfo['name']; 
-    */  
+
     $currentCohort = 'Spring25';
 
     $uid="T02";
@@ -289,7 +241,7 @@
         const uid = "<?= $uid ?>";
         const fullName = "<?= $fullName ?>";
         const currentCohort = "<?= $currentCohort ?>"; 
-        const apiURL = "https://script.google.com/macros/s/AKfycbyuWfg-e2IYAFJHaAi5n67JWPc953pnA0XrJF-uLhUUzJu9TI_Ec8jP5MBvlFMVXqjAOw/exec";
+        const apiURL = "";
         var cartItem=[];
     </script>
     <script type="text/javascript" src="view/activeButton.js"></script>
